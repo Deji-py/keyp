84a74e4d4cab52e8eb4110be8f7a003fad8f16fe
Deji-py <deji-py@github.com>
2022-08-02 15:47:53 +0100

    second commit

--- src/GlobalComp/Form/Keypassword.jsx
+++ src/GlobalComp/Form/Keypassword.jsx
@@ -11,10 +11,12 @@ import { navTheme, ThemeContext } from "../../ThemeContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FormContext } from "../../context/FormContext";
 import { motion } from "framer-motion";
+import done from "../../Asset/Images/Completed.png";
 
 function Keypassword() {
   const { toggleAddkey, setToggleAddkey } = useContext(FormContext);
   const [toggleEye, setToggleEye] = useState();
+  const [keycreated, setkeycreated] = useState(false);
   const { toggleTheme } = useContext(ThemeContext);
   return (
     <div
@@ -23,11 +25,12 @@ function Keypassword() {
         display: toggleAddkey ? "flex" : "none",
       }}
     >
-      <motion.div animate={{
-        scale:toggleAddkey?1:0.8,
-        transition:0.2
-        
-      }}
+      <motion.div
+        animate={{
+          scale: toggleAddkey ? 1 : 0.8,
+
+          transition: 0.2,
+        }}
         className="card AddkeyForm flex__centered"
         style={{
           position: "relative",
@@ -37,7 +40,7 @@ function Keypassword() {
           onSubmit={(e) => {
             e.preventDefault();
           }}
-          style={{...toggleTheme ? navTheme.navDark : navTheme.navlight}}
+          style={{ ...(toggleTheme ? navTheme.navDark : navTheme.navlight) }}
         >
           <button
             className="cancelbtn flex__centered"
@@ -45,35 +48,50 @@ function Keypassword() {
           >
             <FontAwesomeIcon icon={faX} />
           </button>
-          <blockquote className="warning">
-            <FontAwesomeIcon icon={faWarning} style={{ marginRight: "10px" }} />
-            Please note that the key Password must not be shared with a third
-            party
-          </blockquote>
-          <label htmlFor="keypass" style={{ position: "relative" }}>
-            Enter Key Password
-            <input type={toggleEye ? "text" : "password"} name="keypass" />
-            <button
-              className="eyeIcon"
-              style={{ top: "22px" }}
-              onClick={() => setToggleEye(!toggleEye)}
-            >
-              {toggleEye ? (
+          {keycreated === false ? (
+            <>
+              <blockquote className="warning">
                 <FontAwesomeIcon
-                  icon={faEye}
-                  size="lg"
-                  style={{ color: toggleTheme ? "white" : "black" }}
+                  icon={faWarning}
+                  style={{ marginRight: "10px" }}
                 />
-              ) : (
-                <FontAwesomeIcon
-                  icon={faEyeSlash}
-                  size="lg"
-                  style={{ color: toggleTheme ? "white" : "black" }}
-                />
-              )}
-            </button>
-            <div className="createkey ">Create</div>
-          </label>
+                Please note that the key Password must not be shared with a
+                third party
+              </blockquote>
+              <label htmlFor="keypass" style={{ position: "relative" }}>
+                Enter Key Password
+                <input type={toggleEye ? "text" : "password"} name="keypass" />
+                <button
+                  className="eyeIcon"
+                  style={{ top: "22px" }}
+                  onClick={() => setToggleEye(!toggleEye)}
+                >
+                  {toggleEye ? (
+                    <FontAwesomeIcon
+                      icon={faEye}
+                      size="lg"
+                      style={{ color: toggleTheme ? "white" : "black" }}
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faEyeSlash}
+                      size="lg"
+                      style={{ color: toggleTheme ? "white" : "black" }}
+                    />
+                  )}
+                </button>
+                <div className="createkey " onClick={() => setkeycreated(true)}>
+                  Create
+                </div>
+              </label>
+            </>
+          ) : (
+            <div className=" card AddkeyForm flex__centered">
+              <img src={done} alt="done" style={{ width: "100px" }} />
+              <h1>KEY CREATED</h1>
+              <button className="logincta changekey">Change Key</button>
+            </div>
+          )}
         </form>
       </motion.div>
     </div>
